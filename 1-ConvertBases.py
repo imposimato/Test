506d1430762870d7a8b5238c81150a36cdd2d432
Luiz Imposimato Jr <luiz_1982@hotmail.com>
2017-10-11 08:10:41 +0100

    Second

--- 1-ConvertBases.py
+++ 1-ConvertBases.py
@@ -1,34 +1,44 @@
-def conv_base(number, base, result_str =""):
+BASE_CONV = '0123456789abcdef'
 
+def conv_base(number, base):
+
+    result_str = ""
     while number > 0:
 
-        if (base != 2 and base !=8 and base != 16):
-            print("Please inform a base 2, 8 or 16")
+        if 2 > base > len(BASE_CONV):
+            print("Please inform a base up to" + str(len(BASE_CONV)))
             break
 
-        if base == 16:
-            result_str += str(hex(number % base))
-        else:
-            result_str += str(number % base)
+        result_str = str(BASE_CONV[number % base]) + result_str
 
         number //= base
 
-    return result_str[::-1]
+    return result_str
 
 
-def hex(number):
-    return {
-        10: 'A',
-        11: 'B',
-        12: 'C',
-        13: 'D',
-        14: 'E',
-        15: 'F',
-    }.get(number, number)
+def bin_int(bin, base):
+    int_res = 0
+    temp_num = str(bin)
+    power = 0
+    while len(temp_num)>0:
+        count = 0
+        for number in BASE_CONV:
+            if number == temp_num[-1]:
+                break
+            else:
+                count += 1
+
+        int_res += count * (base ** power)
+        power += 1
+        temp_num = temp_num[:-1]
+
+    return int_res
 
 
-base_str = input('Please, inform the base: ')
-base_int = int(base_str)
-number_str = input('Please, inform the number: ')
-number_int = int(number_str)
-print(conv_base(number_int, base_int))
+print(bin_int('aaa',16))
+#print(conv_base(2730,16))
+# base_str = input('Please, inform the base: ')
+# base_int = int(base_str)
+# number_str = input('Please, inform the number: ')
+# number_int = int(number_str)
+# print(conv_base(number_int, base_int))
---
+++ MusicNotation.py
@@ -0,0 +1,27 @@
+def read_music_not(file, mode):
+    count = 0
+    count_x = 0
+    file = open(file, mode)
+    res = open("res.txt", "w")
+    data = file.read().split()
+    for line in data:
+        x = line[2:]
+        y = " ... "
+        if line[:2] == "X:":
+            res.write(x + y)
+            count += 1
+            count_x = 1
+        elif (line[:2]  == "T:" and count_x == 1):
+            res.write(x +y)
+            count_x = 0
+        elif (line[:2] == "M:"):
+            res.write(x + y)
+        elif (line[:2] == "K:"):
+            res.write(x)
+            res.write("\n")
+
+    file.close()
+
+read_music_not("hnr1.txt", "r")
+
+
